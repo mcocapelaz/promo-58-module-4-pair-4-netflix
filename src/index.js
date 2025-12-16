d2bc5550e35692993a5f35ea345dcc5ba9a6ffeb
mercedes223mdp <msbichweiler@gmail.com>
2025-12-16 11:23:33 +0100

    change fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // create and config server
 const server = express();
@@ -10,4 +10,23 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get("/apis/netflix-v1/empty.json", (req, res) => {
+  res.json([
+    {
+      id: "1",
+      title: "Gambita de dama",
+      genre: "Drama",
+      image:
+        "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg",
+    },
+    {
+      id: "2",
+      title: "Friends",
+      genre: "Comedia",
+      image:
+        "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg",
+    },
+  ]);
 });
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -5,26 +5,12 @@ const getMoviesFromApi = () => {
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
     .then(response => response.json())
-    .then(() => {
+    .then((data) => {
+      console.log('data recibida del servidor:', JSON.stringify(data));
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return {
         success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            genre: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            genre: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
+        movies: [data]
       };
     });
 };
