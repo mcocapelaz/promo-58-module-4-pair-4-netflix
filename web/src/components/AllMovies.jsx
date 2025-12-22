0956f8966e418043d31a1b519886c49cec3247d9
Monica Coca <mcocapelaz@gmail.com>
2025-12-22 10:40:06 +0100

    json fixed

--- web/src/components/AllMovies.jsx
+++ web/src/components/AllMovies.jsx
@@ -31,7 +31,7 @@ const AllMovies = ({
             onChange={handleOptions}
           >
             <option value="">Todas</option>
-            <option value="Drama">Drama</option>
+            <option value="Drama">Crimen</option>
             <option value="Comedia">Comedia</option>
           </select>
         </div>
